787175b36ac02f142c9d048bd3db15d2e35c0d09
Stphn002 <teopestephen3@gmail.com>
2024-10-08 11:12:28 +0800

    while loops

---
+++ Loops.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loops Activity</title>
+</head>
+<body>
+    <?php
+        $number = 1;
+            while ($number <=10){
+                echo $number . " ";
+                $number++;
+            }
+    ?>
+    <?php
+        $number = 2;
+            while ($number <=20){
+                echo $number . " ";
+                $number += 2;
+            }
+    ?>
+    
+</body>
+</html>
